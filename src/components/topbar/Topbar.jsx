b717e350d83c14c4f1fb478a268f2a5b649f7b20
Brandon-Magofna <magofna68@gmail.com>
2021-07-21 15:43:26 -0700

    got topbar working correctly with transition

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import { Person, Mail } from "@material-ui/icons";
 
-export default function topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">genius.</a>
@@ -16,7 +16,13 @@ export default function topbar() {
             <span>Magofna68@genius.com </span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span>className="line1"</span>
+            <span>className="line2"</span>
+            <span>className="line3"</span>
+          </div>
+        </div>
       </div>
     </div>
   )
