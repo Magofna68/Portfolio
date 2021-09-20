44a33b7ae97fc7d8bee8f9b4cf94d3fa92af8ec2
Brandon-Magofna <magofna68@gmail.com>
2021-09-20 11:07:02 -0700

    :hammer: out portfolio.js & corresponding style sheet

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,13 @@
 import { ListItem } from "@material-ui/core";
 import "./portfolio.scss"
 import PortfolioList from "../portfolioList/PortfolioList"
-import React, {useState} from 'react';
-import { featuredPortfolio } from "../data";
+import React, {useState, useEffect} from 'react';
+import { featuredPortfolio } from "../data" ;
 
 export default function Portfolio() {
   const [selected, setSelected] = useState("featured");
+  // const [data, setData] = useState();
+
   const [data, setData] = useState([]);
 
   const list = [
@@ -31,7 +33,30 @@ export default function Portfolio() {
     },
  ];
 
- useEffect()
+ useEffect(() => {
+    switch(selected){
+      case "featured": 
+        setData(featuredPortfolio);
+        break;
+      case "mobile": 
+        setData(featuredPortfolio);
+        break;
+      case "web": 
+        setData(featuredPortfolio);
+        break;
+      case "branding": 
+        setData(featuredPortfolio);
+        break;
+      case "featured": 
+        setData(featuredPortfolio);
+        break;
+      case "design":
+        setData(featuredPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+ }, [selected])
 
   return (
     <div className="portfolio" id="portfolio">
@@ -47,38 +72,14 @@ export default function Portfolio() {
           ))}
         </ul>
         <div className="container">
+          {data.map((d) => (
           <div className="item">
-            <img src="https://images.all-free-download.com/images/graphiclarge/apple_coffee_computer_cup_desk_diagram_drink_iphone_597609.jpg"
+            <img src={d.img}
             alt="modern desk with coffee"></img>
-            <h3>Modern App</h3>
+            <h3>{d.title}</h3>
           </div>
-          <div className="item">
-            <img src="https://images.all-free-download.com/images/graphiclarge/apple_coffee_computer_cup_desk_diagram_drink_iphone_597609.jpg"
-            alt="modern desk with coffee"></img>
-            <h3>Modern App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.all-free-download.com/images/graphiclarge/apple_coffee_computer_cup_desk_diagram_drink_iphone_597609.jpg"
-            alt="modern desk with coffee"></img>
-            <h3>Modern App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.all-free-download.com/images/graphiclarge/apple_coffee_computer_cup_desk_diagram_drink_iphone_597609.jpg"
-            alt="modern desk with coffee"></img>
-            <h3>Modern App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.all-free-download.com/images/graphiclarge/apple_coffee_computer_cup_desk_diagram_drink_iphone_597609.jpg"
-            alt="modern desk with coffee"></img>
-            <h3>Modern App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.all-free-download.com/images/graphiclarge/apple_coffee_computer_cup_desk_diagram_drink_iphone_597609.jpg"
-            alt="modern desk with coffee"></img>
-            <h3>Modern App</h3>
-          </div>
+          ))}
         </div>
-
     </div>
-  )
+  );
 }
