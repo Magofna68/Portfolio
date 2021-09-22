528ab81fc1aed9e940a5f1b5ac5934905c8d4781
Brandon-Magofna <magofna68@gmail.com>
2021-09-22 12:38:28 -0700

    complete building out framework for Contact component. Structurally sound, but needs some love

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,27 @@
 import "./contact.scss"
+import React, {useState} from "react";
+
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  }
   return (
     <div className="contact" id="contact">
+      <div className="left">
+        <img src="https://memegenerator.net/img/instances/68186355.jpg" alt="businessmen shaking hands"/>
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email"></input>
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+          {message && <span> Thanks for your interest. <br/>I will get back to you shortly.<br/><br/>    - Brandon</span>}
+      </div>
 
     </div>
   )
