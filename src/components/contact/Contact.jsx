26789ae5b43daebe4adac09218f9abc087267329
Brandon-Magofna <magofna68@gmail.com>
2021-09-27 13:50:57 -0700

    :sparkles: application  aesthetics from a UI perspective

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,26 +12,27 @@ export default function Contact() {
     <div 
     className="contact" 
     id="contact"
-    style={{
-      backgroundRepeat: `no-repeat`,
-      backgroundColor: `black`,
-      backgroundPositionX: `94%`,
-      backgroundPositionY: `5%`,
-      // float: `right`,
-      position: `absolute`,
-      backgroundImage: `url("https://c.tenor.com/EhXBLBx7n9sAAAAd/planet-earth.gif")`
-    }}>
+    // style={{
+    //   backgroundRepeat: `no-repeat`,
+    //   backgroundColor: `black`,
+    //   backgroundPositionX: `94%`,
+    //   backgroundPositionY: `5%`,
+    //   // float: `right`,
+    //   position: `absolute`,
+    //   backgroundImage: `url("https://c.tenor.com/EhXBLBx7n9sAAAAd/planet-earth.gif")`
+    // }}
+    >
       <div className="left">
         <img src="https://memegenerator.net/img/instances/68186355.jpg" alt="businessmen shaking hands"/>
       </div>
       <div className="right">
         <h2>Contact<span>.</span></h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email"></input>
-          <textarea placeholder="Message"></textarea>
+          <input type="text" placeholder="Enter Email.."></input>
+          <textarea placeholder="Enter Message.."></textarea>
           <button type="submit">Send</button>
         </form>
-          {message && <span> Thanks for your interest. <br/>I will get back to you shortly.<br/><br/>    - Brandon</span>}
+          {message && <span> Thanks for your interest. <br/>I will get back to you shortly.<br/><br/>    - Brandon<br/><br/></span>}
       </div>
 
     </div>
