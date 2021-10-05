eae06fd219eaf6124f3d6163954de3f553d111c3
Brandon-Magofna <magofna68@gmail.com>
2021-10-05 14:02:06 -0700

    update aboutMe text fields in component

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -11,24 +11,38 @@ export default function AboutMe() {
       repeat: `no-repeat`,
       backgroundImage: `url("https://i2.wp.com/files.123freevectors.com/wp-content/original/142906-cool-grey-wave-business-background-vector-art.jpg?w=800&q=95")`
     }}>
-    <div 
-    className="container">
+      <div className="container">
       <div className="left">
-        <h1>
+          <h1>
+            <span 
+            style={{
+            color: `grey`
+          }}
+          >About
+          </span> Brandon
           <span 
           style={{
-          color: `grey`
-        }}
-        >About
-        </span> Brandon
-        <span 
-        style={{
-          color: `grey`
-        }}>.</span><br/></h1><br/><br/><br/><br/>
-        <p>Upon graduation in 2019, Brandon dove head first into software sales. After working collaboratively in a sales team with individual goals/quota retirements, he quickly realized how fleeting success was within this industry.</p>
+            color: `grey`
+          }}>.</span><br/></h1><br/><br/><br/><br/><br/>       
+        <p>
+          Upon graduation in 2019, Brandon dove head first into software sales. After working collaboratively in a sales team with individual goals/quota retirements, he quickly realized how fleeting success was within this industry.
+        </p>
         <br/> 
-        <p>After much thought, Brandon came to realize his passion didnt lie in sales, but rather in the excitement of overcoming a challenge. He left my sales role behind and to pursue a career in coding; to be an aspiring software engineer. Applying the knowledge base cultivated from college and from his experiences in sales, Brandon goal is to further develop a growth mindset; leaning into unknown endeavors, and reshaping the world one line of code at a time.</p>
-        
+        <p>
+          After much thought, Brandon came to realize his passion didnt lie in sales, but rather in the excitement of overcoming a challenge. He left my sales role behind and to pursue a career in coding; to be an aspiring software engineer. Applying the knowledge base cultivated from college and from his experiences in sales, Brandon goal is to further develop a growth mindset; leaning into unknown endeavors, and reshaping the world one line of code at a time.
+        </p>
+        <br/>
+        <p>
+          With exposure and repetition to best practices for over the better part of a year, Brandon has been building up his technical and nontechnical skills to place him in a position to overcome complex challenges. 
+          <br/>The strong push for effective, collaborative-work, has honed his interpersonal skills and have granted him insight to alternative perspectives and methodologies while overcoming challenges.
+        </p>
+        <br/>
+        <p>
+          Epicodus has provided a framework for over 250 hours of experience, and nearly 20 projects in each of the following languages: C#/.Net, React, & Javascript. I am currently working as an individual contributor for VALIDATE, LLC, working on a React web application that is nearly ready for its beta launch.
+      </p>
+        {/* <p>
+          While I don't boast to have the most experience writing code, as a representation of myself, I have confidence in leveraging my abilities as a self-starter to learn and fulfill all communicated objectives, while maintaining best practices along the way (GIT, pair-programming, CRUD, REST)
+        </p> */}
       </div>
       <div className="right">
         <div className="clickables">
@@ -54,6 +68,13 @@ export default function AboutMe() {
           </div>
         </div>
         </div>
+      <div className="center">
+      <p style={{
+          fontWeight: `500`
+        }}>
+          While I don't boast to have the most experience writing code, as a representation of myself, I have confidence in leveraging my abilities as a self-starter to learn and fulfill all communicated objectives, while maintaining best practices along the way (GIT, pair-programming, CRUD, REST)
+        </p>
+      </div>
       </div>
     </div>
   )
