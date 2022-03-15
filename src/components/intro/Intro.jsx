c3959c59fcbcc58a3fae14eccd3ca25c941605d7
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-03-15 16:28:33 -0700

    update intro page to have bootstrap responsivness

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,6 +5,9 @@ import { useEffect, useRef } from "react";
 import Placeholder from "../../assets/placeholder.JPG";
 import Arrow from "../../assets/arrow.png";
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
 
@@ -26,48 +29,74 @@ export default function Intro() {
   }, []);
 
   return (
-    <div 
-      className="intro" 
-      id="intro"
-      style={{ 
-        backgroundImage: `url("https://cdn.wallpapersafari.com/83/6/DY9RHb.gif")`,
-        backgroundRepeat: `no-repeat`,
-        justifyContent: `center`,
-        backgroundColor: `black`,
-        opacity: `90%`,
-        backgroundSize: `cover`
-        
-      }}>
-      <div className="left">
-        <div 
-          className="imgContainer"
-            style={{
-            backgroundImage: `url("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX25806764.jpg")`,
-            opacity: '80%',
-          }}
-          >
-            <Image src={Placeholder} fluid="true" alt="Profile Picture"></Image>
-          {/* <img src={Placeholder} alt="test"></img> */}
-        </div>
-      </div>
-      <div className="right">
-        <div className="wrapper">
-          <h2>Hi There, I'm </h2>
-          <h1>Brandon Magofna.</h1>
-          <h3><span ref={textRef}></span></h3>
-        </div>
-        <div className="wrapper">
-          <a href="#aboutMe">
-            <DoubleArrowIcon className="downIcon" onClick="" />
-          </a>
-        </div>
-      </div>
-      <div className="mobileArrow">
-        <a href="#aboutMe">
-          <DoubleArrowIcon className="downIconMobile" onClick="" />
-        </a>
-      </div>
+    <div id="introPage">
+      <Container fluid>
+        <Row>
+          <Col>
+            <div 
+              id="imgContain"
+              style={{
+              backgroundImage: `url("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX25806764.jpg")`,
+              opacity: '80%',
+              maxWidth: `600px`,
 
+              }}
+              >
+              <div 
+                styles={{
+                  width
+                }}>
+                <Image src={Placeholder} fluid alt="Profile Picture"></Image>
+              </div>
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+
+      </Container>
     </div>
+
+    // <div 
+    //   className="intro" 
+    //   id="intro"
+    //   style={{ 
+    //     backgroundImage: `url("https://cdn.wallpapersafari.com/83/6/DY9RHb.gif")`,
+    //     backgroundRepeat: `no-repeat`,
+    //     justifyContent: `center`,
+    //     backgroundColor: `black`,
+    //     opacity: `90%`,
+    //     backgroundSize: `cover`
+        
+    //   }}>
+    //   <div className="left">
+    //     <div 
+    //       className="imgContainer"
+    //         style={{
+    //         backgroundImage: `url("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX25806764.jpg")`,
+    //         opacity: '80%',
+    //       }}
+    //       >
+    //         <Image src={Placeholder} fluid alt="Profile Picture"></Image>
+    //       {/* <img src={Placeholder} alt="test"></img> */}
+    //     </div>
+    //   </div>
+    //   <div className="right">
+    //     <div className="wrapper">
+    //       <h2>Hi There, I'm </h2>
+    //       <h1>Brandon Magofna.</h1>
+    //       <h3><span ref={textRef}></span></h3>
+    //     </div>
+    //     <div className="wrapper">
+    //       <a href="#aboutMe">
+    //         <DoubleArrowIcon className="downIcon" onClick="" />
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div className="mobileArrow">
+    //     <a href="#aboutMe">
+    //       <DoubleArrowIcon className="downIconMobile" onClick="" />
+    //     </a>
+    //   </div>
+    // </div>
   )
 }
