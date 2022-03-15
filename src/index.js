4da3a2dbbb5308100d21e828a41f4b29024f9454
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-03-15 16:28:33 -0700

    update intro page to have bootstrap responsivness

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+// import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
