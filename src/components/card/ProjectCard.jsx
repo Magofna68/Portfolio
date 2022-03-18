f227dc1ad170f2ff3296e55443eeb909c0fca9a1
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-03-18 16:41:28 -0700

    add code to render photo's correctly in deployed application

--- src/components/card/ProjectCard.jsx
+++ src/components/card/ProjectCard.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import "./card.scss";
 
 export default function ProjectCard({title, link, subtitle, alt, image, text}) {
+  // export default function renderCard = (card, index) => {
   return (
-    // <Container>
-      <Card style={{
-        width: '18rem'
-      }}>
-        <Card.Img 
-          variant="top"
-          src={image}
-          alt={alt} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
-          <Card.Text>{text}</Card.Text>
-          <Card.Link href={link} rel="noreferrer">Check out this Project</Card.Link>
-          // <Card.Link href="#" rel="noreferrer">View Project's Code</Card.Link>
-        </Card.Body>
-      </Card>
-    // </Container>
+    <Card style={{
+      width: '18rem'
+    }}>
+      <Card.Img 
+        variant="top"
+        src={image}
+        alt={alt} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
+        <Card.Text>{text}</Card.Text>
+        <Card.Link href={link} rel="noreferrer">Check out this Project</Card.Link>
+      </Card.Body>
+    </Card>
   )
 };
