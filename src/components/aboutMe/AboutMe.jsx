d4110e9ac9504b0855e391af47ca94e1fbe5440a
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-03-23 12:02:41 -0700

    connect scss file to AboutMe component

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import aboutMe from "./aboutMe.scss";
+// import "./aboutMe.scss"
 import Resume from "../../assets/Resume.png";
 import { Container, Row, Col } from 'react-bootstrap';
 
